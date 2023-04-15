af4f91c5c2104a949838838170a6bda32ccb75d4
Ubuntu <azureuser@DeepLearning.k1tjldqbbwtuzau2z4zr2ovjkf.cx.internal.cloudapp.net>
2023-04-15 12:18:27 +0000

    Add list of dog types

--- src/demo.cpp
+++ src/demo.cpp
@@ -10,17 +10,53 @@ using namespace cv;
 int main()
 {
     int batch_size = 16;
+    bool visualize = true;
 
     std::filesystem::path datapath = std::filesystem::current_path() / "data/images";
 
     std::unordered_map<std::string, int> classes = {
-        {"cats", 0},
-        {"dogs", 1}
+        {"Abyssinian", 0},
+        {"american_bulldog", 1},
+        {"american_pit_bull_terrier", 2},
+        {"basset_hound", 3},
+        {"beagle", 4},
+        {"Bengal", 5},
+        {"Birman", 6},
+        {"Bombay", 7},
+        {"boxer", 8},
+        {"British_Shorthair", 9},
+        {"chihuahua", 10},
+        {"Egyptian_Mau", 11},
+        {"english_cocker_spaniel", 12},
+        {"english_setter", 13},
+        {"german_shorthaired", 14},
+        {"great_pyrenees", 15},
+        {"havanese", 16},
+        {"japanese_chin", 17},
+        {"keeshond", 18},
+        {"leonberger", 19},
+        {"Maine_Coon", 20},
+        {"miniature_pinscher", 21},
+        {"newfoundland", 22},
+        {"Persian", 23},
+        {"pomeranian", 24},
+        {"pug", 25},
+        {"Ragdoll", 26},
+        {"Russian_Blue", 27},
+        {"saint_bernard", 28},
+        {"samoyed", 29},
+        {"scottish_terrier", 30},
+        {"shiba_inu", 31},
+        {"Siamese", 32},
+        {"Sphynx", 33},
+        {"staffordshire_bull_terrier", 34},
+        {"wheaten_terrier", 36},
+        {"yorkshire_terrier", 36},
     };
 
     String path_string = String(datapath.string());
 
-    PetDataset dataset{path_string, classes};
+    auto dataset = PetDataset(path_string, classes).map(torch::data::transforms::Stack<>());
 
     // Generate a data loader.
     auto data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
@@ -28,11 +64,23 @@ int main()
         batch_size
     );
 
-    for (auto& batch : *data_loader) {
-        auto data = batch[0].data;
-        auto labels = batch[0].target;
+    if (visualize) {
+        auto& batch = *data_loader
 
-        std::cout << labels << std::endl;
+        auto data = batch.data;
+        auto labels = batch.target;
+
+        std::cout << "hi" << std::endl;
+
+        return 0
+    }
+
+    for (auto& batch : *data_loader) {
+        auto data = batch.data;
+        auto labels = batch.target;
+
+        std::cout << data << std::endl;
+
     }
 
     return 0;
